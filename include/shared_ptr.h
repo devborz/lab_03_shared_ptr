51dfec063ef22aa56e4458e28fd9b3b566b1194c
Usman Turkaev <u.turkaev@yandex.ru>
2020-01-09 19:19:14 +0300

    Updated

---
+++ include/shared_ptr.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <atomic>
+#include <iostream>
+
+template <typename T>
+class shared_ptr {
+ public:
+  shared_ptr() = default;
+  shared_ptr(T* ptr);
+  shared_ptr(const shared_ptr& r);
+  shared_ptr(shared_ptr&& r);
+  ~shared_ptr();
+  shared_ptr<T>& operator=(const shared_ptr& r);
+  shared_ptr<T>& operator=(shared_ptr&& r);
+  operator bool() const;
+  T& operator*();
+  T* operator->();
+  T* release();
+  T* get();
+  void reset(T* ptr = nullptr);
+  void swap(shared_ptr& r);
+  std::atomic_uint* use_count();
+
+ private:
+  T* pointer_;
+  std::atomic_uint* use_count_;
+};
+
+template <typename T>
+shared_ptr<T>::shared_ptr(T* ptr) {
+  this->pointer_ = ptr;
+  this->use_count_ = new std::atomic_uint(1);
+}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(const shared_ptr& r) {  // констурктор копирования
+  this->pointer_ = r.pointer_;
+  this->use_count_ = r.use_count_;
+  ++*this->use_count_;
+}
+
+template <typename T>
+shared_ptr<T>::shared_ptr(shared_ptr&& r) {  // констурктор перемещения
+  this->swap(r);
+}
+
+template <typename T>
+shared_ptr<T>::~shared_ptr() {
+  this->reset();
+}
+
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr& r) {
+  shared_ptr(r).swap(*this);  // создает временный указатель shared,
+  // который копирует r, меняет значение this с ним
+  return *this;  // возвращает полученное значение
+}
+
+template <typename T>
+shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr&& r) {
+  shared_ptr(std::move(r)).swap(*this);  // создает временный указатель shared,
+  // который копирует r, меняет значение this с ним
+  return *this;  // возвращает полученное значение
+}
+
+template <typename T>
+shared_ptr<T>::operator bool() const {
+  return (this->pointer_ != nullptr);
+  // проверяет, указывает ли указатель на объект
+}
+
+template <typename T>
+T& shared_ptr<T>::operator*() {
+  return *this->pointer_;  // возвращает значение по ссылке
+}
+
+template <typename T>
+T* shared_ptr<T>::operator->() {
+  return this->pointer_;  // возвращает указатель
+}
+
+template <typename T>
+T* shared_ptr<T>::release() {
+  auto temp = shared_ptr(*this);  // сохраняет указатель
+  this->reset();                  // сбрасывает shared_ptr
+  return temp.get();              // возвращает указатель
+}
+
+template <typename T>
+T* shared_ptr<T>::get() {
+  return this->pointer_;  // возвращает указатель на объект
+}
+
+template <typename T>
+void shared_ptr<T>::reset(T* ptr) {
+  // если this указывает на объект, уменьшает значение счетчика
+  if (this->use_count_ != nullptr)
+    if (*this->use_count_ == 1) {
+      delete this->use_count_;
+      // delete this->pointer_;
+    } else {
+      --*this->use_count_;
+    }
+
+  this->pointer_ = ptr;
+
+  if (ptr != nullptr) this->use_count_ = new std::atomic_uint{1};
+}
+
+template <typename T>
+void shared_ptr<T>::swap(shared_ptr& r) {
+  std::swap(this->pointer_, r.pointer_);
+  std::swap(this->use_count_, r.use_count_);
+}
+
+template <typename T>
+std::atomic_uint* shared_ptr<T>::use_count() {
+  return this->use_count_;  // возвращает значение счетчика
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
-#include "SharedPtr.h"
+#include "shared_ptr.h"
 
-using std::cout;
 using std::cin;
+using std::cout;
 
 int main() {
-    int x = 2;
-    int y = 4;
-    int z = 8;
+  shared_ptr<int> ptr1(new int{1});
+  shared_ptr<int> ptr2(new int{2});
+  shared_ptr<int> ptr3(ptr2);
+  shared_ptr<int> ptr4;
+  try {
+    cout << "\nPtr1 = " << *ptr1.get() << " cnt = " << *ptr1.use_count();
+    cout << "\nPtr2 = " << *ptr2.get() << " cnt = " << *ptr2.use_count();
+    cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
 
-    SharedPtr<int> ptr1(&x);
-    SharedPtr<int> ptr2(&y);
-    SharedPtr<int> ptr3(ptr2);
-    SharedPtr<int> ptr4;
-    try {
-        cout << "\nPtr1 = " << *ptr1.get() << " cnt = " << *ptr1.use_count();
-        cout << "\nPtr2 = " << *ptr2.get() << " cnt = " << *ptr2.use_count();
-        cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
+    cout << "\n\nPtr3 was copied from Ptr2. Now we will reset Ptr2 to check"
+         << " if Ptr3's сounter points to nowhere...\nRESETTING PTR2...";
+    ptr2.reset();
+    cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
 
-        cout << "\n\nPtr3 was copied from Ptr2. Now we will reset Ptr2 to check"
-        << " if Ptr3's сounter points to nowhere...\nRESETTING PTR2...";
-        ptr2.reset();
-        cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
+    ptr1.swap(ptr3);
 
-        ptr1.swap(ptr3);
+    cout << "\n\nPtr1 and Ptr3 swap:";
+    cout << "\nPtr1 = " << *ptr1.get();
+    cout << "\nPtr3 = " << *ptr3.get();
 
-        cout << "\n\nPtr1 and Ptr3 swap:";
-        cout << "\nPtr1 = " << *ptr1.get();
-        cout << "\nPtr3 = " << *ptr3.get();
+    cout << "\n\nPtr3 reset for new:\n";
+    ptr3.reset(new int{5});
+    cout << "Ptr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
 
-        cout << "\n\nPtr3 reset for varable Z:\n";
-        ptr3.reset(&z);
-        cout << "Ptr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count();
-
-        cout << "\n\nDefine of Ptr4 (Ptr4 = Ptr3):";
-        ptr4 = ptr3;
-        cout << "\nPtr4 = " << *ptr4.get() << " cnt = " << *ptr4.use_count();
-        cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count()
-        << std::endl;
-
-    }
-    catch (const std::exception& e) {
-        cout << "\n" << e.what();
-    }
+    cout << "\n\nDefine of Ptr4 (Ptr4 = Ptr3):";
+    ptr4 = ptr3;
+    cout << "\nPtr4 = " << *ptr4.get() << " cnt = " << *ptr4.use_count();
+    cout << "\nPtr3 = " << *ptr3.get() << " cnt = " << *ptr3.use_count()
+         << std::endl;
+  } catch (const std::exception& e) {
+    cout << "\n" << e.what();
+  }
 }
